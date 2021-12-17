8e7d1f596501d980e6fa9b3177e54bbe63088b0b
wajeehafarids911 <wajeehafarids@gmail.com>
2021-12-17 17:24:24 +0000

    State filtering working on dealership.

--- server/api/views.py
+++ server/api/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import DealershipRestSerializer, ReviewRestSerializer
 from .models import DealershipRest, ReviewRest
+import json
+
+
+@api_view(["GET"])
+def get_dealership_with_state(request):
+    if "state" in request.GET:
+        state_i = request.GET.get("state")
+        dealerships_list = DealershipRest.objects.all().filter(st=state_i)
+        serializer_obj = DealershipRestSerializer(dealerships_list, many=True)
+        return HttpResponse(json.dumps(serializer_obj.data, indent=2), content_type="application/json")
+        print(f"Found state: {state_i}")
+    else:
+        print("State not given in field...")
+    try:
+        dealer_obj = DealershipRest.objects.get(st=state_code)
+        print(f"Found objects for state={state_code}")
+    except:
+        pass
+
+    return render(request, 'djangoapp/about.html')
+
 
 @api_view(["GET"])
 def get_api_view(request):
@@ -53,10 +74,4 @@ def del_dealership(request, pk):
     except:
         return Response(f"Dealership does not exist for dealerId={pk}")
 
-def get_dealership_with_state(request, state_code):
-    try:
-        dealer_obj = DealershipRest.objects.get(st=state_code)
-        print(f"Found objects for state={state_code}")
-    except:
-        return render(request, 'djangoapp/about.html')
 
