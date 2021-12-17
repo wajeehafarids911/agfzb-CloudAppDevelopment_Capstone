29aa2ad16cdf914eee67311a2ccb4d1072f62453
Wajeeha Sheikh <wajeehafarids@gmail.com>
2021-12-17 12:21:29 +0000

    Fixes to make the cloudant calls work.

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -14,6 +14,7 @@ import requests
 
 def main(dict):
     databaseName = "dealerships"
+    print("Hello")
 
     try:
         client = Cloudant.iam(
@@ -22,6 +23,17 @@ def main(dict):
             connect=True,
         )
         print("Databases: {0}".format(client.all_dbs()))
+        dbs_output = client["dealerships"]
+        type_of_dbs_output = type(dbs_output)
+        print(f"type-of-dealerships: {type_of_dbs_output}, dealerships: {dbs_output}")
+        
+        print("Count:", dbs_output.doc_count())
+        keys_in_dict = dbs_output.keys(remote=True)
+        for key_i in keys_in_dict:
+            value_i = dbs_output.get(key_i, remote=True)
+            print(f"({key_i} : {value_i})")
+        print(dbs_output)
+
     except CloudantException as ce:
         print("unable to connect")
         return {"error": ce}
@@ -30,3 +42,13 @@ def main(dict):
         return {"error": err}
 
     return {"dbs": client.all_dbs()}
+
+
+###########################
+if __name__ == '__main__':
+    login_dict = {
+        "COUCH_URL": "https://9a8f2c7b-c8c1-41e1-988e-182c3f5d926f-bluemix.cloudantnosqldb.appdomain.cloud",
+        "IAM_API_KEY": "YE7undQzSOjWO9FYgY3AIpv7GxGvr9yhL1prUay-HWPv",
+        "COUCH_USERNAME": "9a8f2c7b-c8c1-41e1-988e-182c3f5d926f-bluemix"
+    }
+    main(login_dict)
