72acb2db4c830d806ad6086c894d4f5137af8047
wajeehafarids911 <wajeehafarids@gmail.com>
2021-12-18 13:30:20 +0000

    UPDATED

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -218,7 +218,10 @@ def register_user(request):
             login(request, user)
         
         # Return to main page after logging or signing up the user
-    return get_dealerships(request)
+        return get_dealerships(request)
+
+    else: # If it is a GET request, just render the registration page
+        return render(request, 'djangoapp/registration.html', context)
 
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
